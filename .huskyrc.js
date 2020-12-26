1b6ca03b5db487964cd774c58ccf848bcc6cff2f
Zy-on <rodriguessilvadrs@gmail.com>
2020-12-26 10:41:52 -0300

    feat(authroutes/configs): creating some basic routes, pages and configs

---
+++ .huskyrc.js
@@ -0,0 +1,12 @@
+// const tasks = arr => arr.join(' && ');
+
+// module.exports = {
+//   hooks: {
+//     'pre-commit': tasks([
+//       'git-branch-is -r "^((?!main).)*$"',
+//       'git-branch-is -r "^((?!develop).)*$"',
+//       'lint-staged',
+//     ]),
+//     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+//   },
+// };
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Home: React.FC = () => <Title>Bem vindo ao GuiaSped</Title>;
+
+export default Home;
